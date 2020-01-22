17bd627a30e5344541b60a0f3724e9e7b6086dda
renn_08 <renn_08@outlook.com>
2020-01-23 00:32:43 +0800

    2020.1.23.0.32

--- ComputeBMI.m
+++ ComputeBMI.m
@@ -2,16 +2,16 @@ function[BMI,status]=ComputeBMI(weight_kg, height_cm)
 BMI=round(weight_kg/(height_cm/100)^2,1);                    
 if BMI <18.5                                                              
     status=1;
-    fprintf('Underweight!');%Underweight
+    fprintf('Underweight!');
 elseif BMI<=24.9
     status=2;
-    fprintf('Normal or Healthy Weight!');%Normal or Healthy Weight
+    fprintf('Normal or Healthy Weight!');
 elseif BMI<=29.9
     status=3;
-    fprintf('Overweight!');%Overweight
+    fprintf('Overweight!');
 elseif 30.0<= BMI
     status=4;
-    fprintf('Obese!');%Obese
+    fprintf('Obese!');
 end
 
 

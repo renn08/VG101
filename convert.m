ff20141f128f5aa898539713916fb60a62f3f2ba
renn_08 <renn_08@outlook.com>
2020-01-23 00:29:40 +0800

    2020.1.23.0.29

--- convert.m
+++
@@ -1,3 +0,0 @@
-function NUM=convert(num,from,to)
-NUM=convertTo(convertFrom(num,from),to);
-end
--- convertTo.m
+++
@@ -1,8 +0,0 @@
-function numB=convertTo(num10,Base)
- L=floor(log(num10)/log(Base))+1;
- numB=0;
- for i=1:L
-     b=mod(num10,Base);
-     num10=(num10-b)/Base;
-     numB=numB+b*10^(i-1);
-end

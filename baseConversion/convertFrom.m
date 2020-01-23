9008853bb6ed5c7517b1fcd27d6f85766784a1ba
renn_08 <renn_08@outlook.com>
2020-01-23 09:43:24 +0800

    2020.1.23.9.43

--- baseConversion/convertFrom.m
+++ baseConversion/convertFrom.m
@@ -1,6 +1,6 @@
 function numT = convertFrom(numB, Base)%十进制及以下变换
 numT=0;
-for i=1:floor(log10(numB)+1)                      %数的长度还用fix()或length(num2str(numB))经试验第一种更快
+for i=1:floor(log10(numB)+1)%数的长度还用fix()或length(num2str(numB))经试验第一种更快
  re=mod(numB,10);
  numB=(numB-re)/10;
  numT=re*Base^(i-1)+num10;
